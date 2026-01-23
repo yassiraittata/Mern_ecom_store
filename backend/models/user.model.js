97d09d74fa1a5f7534952c6796ec891c395b9c2d
Yassir aittata <yassir.aittata@gmail.com>
2026-01-23 16:54:39 +0100

    create user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -39,3 +40,25 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+const User = mongoose.model("User", userSchema);
+export default User;
+
+// Pre-save hook to hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare entered password with hashed password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
