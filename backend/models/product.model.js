995b1924558e8bb7d0db94a06495dc0f119575af
Yassir aittata <yassir.aittata@gmail.com>
2026-01-24 16:30:36 +0100

    product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  }, 
+});
+
+export default mongoose.model("Product", productSchema);
