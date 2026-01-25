f1bb963ba0cbbd32a0eb9c19e49b386b5680967e
Yassir aittata <yassir.aittata@gmail.com>
2026-01-25 15:42:05 +0100

    Get featured products from redis

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,6 @@
+import createHttpError from "http-errors";
 import product from "../models/product.model.js";
+import redis from "../lib/redis.js";
 
 export const getAllProducts = async (req, res, next) => {
   const products = await product.find({});
@@ -10,7 +12,22 @@ export const getAllProducts = async (req, res, next) => {
 };
 
 export const getFeaturedProducts = async (req, res, next) => {
-  const featuredProducts = await product.find({ isFeatured: true });
+  // get data from redis cache
+  let featuredProducts = await redis.get("featured_products");
+  if (featuredProducts) {
+    return res.status(200).json({
+      success: true,
+      featuredProducts: JSON.parse(featuredProducts),
+    });
+  }
+  // get data from mongo db
+  featuredProducts = await product.find({ isFeatured: true }).lean();
+  if (!featuredProducts) {
+    return next(createHttpError(404, "No featured products found"));
+  }
+
+  // store data in redis cache
+  await redis.set("featured_products", JSON.stringify(featuredProducts));
   res.status(200).json({
     success: true,
     featuredProducts,
