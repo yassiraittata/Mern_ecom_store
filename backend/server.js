c5bc1451a652f8f75ec21f3f8f6aac3430dcbff5
Yassir aittata <yassir.aittata@gmail.com>
2026-02-26 14:15:22 +0000

    create payment routes

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 const app = express();
 const PORT = env.PORT || 5000;
@@ -23,6 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.use((req, res, next) => {
   next(createHttpError(404, "Endpoint was not Found"));
