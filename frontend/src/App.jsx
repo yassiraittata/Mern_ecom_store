ea060ff7eb5a9b5c0bdfdca6e51fe2ee4bc5d29e
Yassir aittata <yassir.aittata@gmail.com>
2026-02-28 15:35:39 +0000

    create routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/home",
-    element: <p>Home Page</p>,
-  },
-  {
-    path: "/about",
-    element: <p>About Page</p>,
-  },
-]);
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import NavBar from "./components/NavBar";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden ">
+      {/* Background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+        </div>
+      </div>
+      <div className="relative z-50 pt-20">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+      <Toaster />
+    </div>
+  );
 };
 
 export default App;
