6188cc3645ee58871e39b515f439500cb0272048
Yassir aittata <yassir.aittata@gmail.com>
2026-02-28 12:07:24 +0000

    finsih backend

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,4 +1,4 @@
-import Order from "../models/order.model";
+import Order from "../models/order.model.js";
 import Product from "../models/product.model.js";
 import User from "../models/user.model.js";
 
