98ad50a6526f2df457fbb6d51b848fa145ec39bb
Yassir aittata <yassir.aittata@gmail.com>
2026-02-28 12:47:54 +0000

    cretae router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <p>Home Page</p>,
+  },
+  {
+    path: "/about",
+    element: <p>About Page</p>,
+  },
+]);
 
 const App = () => {
-  return <div className="bg-red-600">App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
