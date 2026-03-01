fef70e3ae383bc228cdff2eed6c19fa9cf242d79
Yassir aittata <yassir.aittata@gmail.com>
2026-03-01 14:46:59 +0000

    handle errors

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
